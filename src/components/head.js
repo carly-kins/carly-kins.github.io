cd57667a2deb015bebb4280fa927cfec26da8760
Carly Wooten <wooten.carly@me.com>
2026-04-15 22:27:56 -0500

    quick order fix

--- src/components/head.js
+++ src/components/head.js
@@ -8,11 +8,11 @@ export function Head( {location, params, data, pageContext} ) {
 		<>
 			<html lang="en" />
 			<title>Carly Wooten Gall</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 			<link rel="preconnect" href="https://fonts.googleapis.com"></link>	
 			<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin></link>
 			<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet"></link>
 			<link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet"></link>
-			<meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 		</>
 	);
 }
